fa708c9b24c27cbc400c899467ea354e020cb468
readysloth <readysloth@protonmail.com>
2021-10-25 18:39:36 +0300

    add(build_image.sh): Added alpine image preparation

---
+++ build_image.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+type_in() {
+  for c in $(echo "$@" | sed 's/./& /g' | sed 's/*/\\*/g' )
+  do
+    case "$c" in
+      '%') c=spc ;;
+      '-') c=minus ;;
+      '/') c=slash ;;
+      '.') c=dot ;;
+      '"') c=apostrophe ;;
+      '\*') c=asterisk ;;
+      '_') echo "sendkey shift-minus" | nc -q0 127.0.0.1 $MONITOR_PORT && continue ;;
+      '~') DO_UPPERCASE=yes && continue ;;
+    esac
+
+    [ "$DO_UPPERCASE" == yes ] && DO_UPPERCASE= && c="shift-$c"
+
+    echo "sendkey $c" | nc -q0 127.0.0.1 $MONITOR_PORT
+  done
+  echo "sendkey kp_enter" | nc -q0 127.0.0.1 $MONITOR_PORT
+}
+
+#sh download.sh
+DISK=hdd.img
+MONITOR_PORT=9999
+
+
+qemu-img create -f qcow2 $DISK 8G
+qemu-system-x86_64 \
+  -enable-kvm \
+  -m 1024 \
+  -cdrom *.iso \
+  -hda $DISK \
+  -netdev user,id=ssh_net,hostfwd=tcp:127.0.0.1:12222-:22 \
+  -device e1000,netdev=ssh_net \
+  -monitor tcp:127.0.0.1:$MONITOR_PORT,server,nowait > /dev/null &
+
+sleep 20
+type_in root
+type_in apk%add%openssh
+type_in sed%-i%'"s/.*~permit~root~login.*/~permit~root~login%yes/g"'%/etc/ssh/sshd_config
+type_in sed%-i%'"s/.*~permit~empty~passwords.*/~permit~empty~passwords%yes/g"'%/etc/ssh/sshd_config
+type_in rc-service%sshd%start
